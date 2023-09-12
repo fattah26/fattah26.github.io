23eccc033d3513957df2247efbda4efb54447c62
FattahArif <fattaharif87@gmail.com>
2023-09-12 20:09:41 +0700

    Pembaruan Navbar

--- script.js
+++ script.js
@@ -12,14 +12,73 @@ window.onscroll = function(){
     }
 };
 
+
 // Hamburger
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
+// Tambahkan event listener ke dokumen
+document.addEventListener('click', function (event) {
+    // Periksa apakah yang diklik adalah hamburger atau salah satu elemen anaknya
+    const isHamburgerClicked = event.target === hamburger || hamburger.contains(event.target);
+
+    // Periksa apakah menu sedang terbuka
+    const isNavMenuOpen = !navMenu.classList.contains('hidden');
+
+    if (!isHamburgerClicked && isNavMenuOpen) {
+        // Jika yang diklik di luar hamburger dan menu terbuka, sembunyikan menu dengan animasi
+        hamburger.classList.remove('hamburger-active');
+        navMenu.style.opacity = '0';
+        navMenu.style.transform = 'translateX(100%)';
+        setTimeout(() => {
+            navMenu.classList.add('hidden');
+        }, 300); // Sesuaikan dengan durasi transisi CSS (0.3s)
+    }
+});
+
 hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+    if (navMenu.classList.contains('hidden')) {
+        // Jika menu sedang tertutup, buka menu dengan animasi dari kanan ke kiri
+        navMenu.classList.remove('hidden');
+        setTimeout(() => {
+            navMenu.style.opacity = '1';
+            navMenu.style.transform = 'translateX(0)';
+        }, 0); // Waktu kecil untuk memungkinkan penampilan elemen sebelum animasi dimulai
+    } else {
+        // Jika menu sedang terbuka, tutup menu dengan animasi ke kanan
+        navMenu.style.opacity = '0';
+        navMenu.style.transform = 'translateX(100%)';
+        setTimeout(() => {
+            navMenu.classList.add('hidden');
+        }, 300); // Sesuaikan dengan durasi transisi CSS (0.3s)
+    }
 });
+
+
+
+// // Tambahkan event listener untuk mengamati scroll
+// window.addEventListener('scroll', function () {
+//     const scrollY = window.scrollY;
+    
+//     // Atur opacity dan backdrop-filter sesuai dengan posisi scroll
+//     if (scrollY > 0) {
+//         navMenu.style.opacity = '0.7'; // Sesuaikan sesuai kebutuhan
+//         navMenu.style.backdropFilter = 'blur(3px)'; // Sesuaikan sesuai kebutuhan
+//     } else {
+//         navMenu.style.opacity = '1'; // Kembalikan opacity normal ketika scroll ke atas
+//         navMenu.style.backdropFilter = 'none'; // Hapus efek blur ketika scroll ke atas
+//     }
+// });
+
+
+
+
+
+
+
+
+
 
 
 
